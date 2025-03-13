f042ab24a4893edcb81a1da20c4cc03ad65a0169
zvereva <96833638+zvereva-s@users.noreply.github.com>
2025-03-13 21:27:28 +0200

    Update page.tsx

--- fe/src/app/page.tsx
+++ fe/src/app/page.tsx
@@ -12,10 +12,10 @@ type Props = {
 export default async function Home({params}:Props) {
     const recipes: Recipe[] = await RecipesService.getRecipes();
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-2 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
           <Container>
-              <h1 className="text-3xl sm:text-4xl lg:text-5xl font-bold tracking-tight text-foreground border-b border-border pb-3 text-center">
+              <h1 className="sticky top-0 left-0 w-full bg-background text-3xl sm:text-4xl lg:text-5xl font-bold tracking-tight text-foreground border-b border-border py-4 text-center z-50">
                 All Recipes
               </h1>
               <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 p-4">
